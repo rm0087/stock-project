b63d44094d59fc33b91d7bb75a8f0a8d1cfba9f6
rm0087 <michetti.ray@gmail.com>
2024-06-18 17:41:45 -0400

    post-class

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ async function getKeywordList(){
         const response = await fetch("http://localhost:3000/keywords");
         const keywords = await response.json();
         keywords.forEach((keyword)=>{
+            //getStocks();
             createNewCheckBox(keyword)});
     } catch (error) {
         console.error(error);
@@ -67,13 +68,60 @@ function createNewCheckBox(checkboxKeyword){
 
 const modeInput = document.querySelector("#mode-input");
 modeInput.addEventListener("input", (event)=>{
-    console.log(event.target.value)
+    const tickerInput = document.querySelector("#ticker-input");
+    const keywordInput = document.querySelector("#keyword-input");
+    if (modeInput.value === "stocks-option"){
+        keywordInput.disabled = true; tickerInput.disabled = false;
+    } else {tickerInput.disabled = true; keywordInput.disabled = false}
+    console.log(modeInput.value)
+})
+
+const stockInput = document.querySelector("#ticker-form")
+stockInput.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    event.target.value = document.querySelector("#ticker-input").value;
+    getStocks(event);
 })
 
 document.querySelector("#keyword-h2").addEventListener("click", (event)=>{
     alert("Please select a keyword(s)")
 })
 
+async function getStocks(event){
+    try {
+        const response = await fetch("http://localhost:3001/companies");
+        const companies = await response.json();
+    
+        selectedCompany = companies.find((company)=> company.ticker.toString().toLowerCase() === event.target.value.toString().toLowerCase());
+        document.querySelector("#stock-info").textContent = selectedCompany.name.toString();
+        
+        stockInput.reset();
+    } catch (error) {
+        console.error(error);
+    } 
+}
+
+let selectedCompany={};
+
+const applyKeywords = document.querySelector("#keywords-form")
+applyKeywords.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    pushKeywords(event)
+})
+    
+function pushKeywords(event){
+    selectedCompany.keywords = [];
+    let nodeList = document.querySelectorAll('input[type=checkbox]:checked');
+    for (let i=0;i<nodeList.length;i++){
+        let item = nodeList[i];
+        if (selectedCompany.ticker != undefined){
+            selectedCompany.keywords.push(item.value.toString())
+        }else{}
+    }
+    applyKeywords.reset();
+    document.querySelector("#stock-keywords").textContent = selectedCompany.keywords;
+    console.log(selectedCompany.keywords)
+}
 
 
 
